d16d24933d6d95024900523ba82cfe22986c8e36
edk21 <edwinmcken@gmail.com>
2023-08-26 14:05:46 +0200

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,129 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { IoIosTrash } from 'react-icons/io';
-import ReactHTMLTableToExcel from 'react-html-table-to-excel';
-import Footer from './Footer';
-import Spinner from './Spinner';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { IoIosTrash } from "react-icons/io";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
+import Footer from "./Footer";
+import Spinner from "./Spinner";
 
 const Stats = () => {
-    const [childs, setChild] = useState([]);
-    const [filteredData, setFilteredData] = useState(childs);
-    //console.log("childs: ", childs)
-
-    const getAllStats = async () => {
-      await axios
-        .get('https://speelplenapi.onrender.com/record/stats/')
-        .then((response) => {
-          setChild(response.data);
-          setFilteredData(response.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-    const deleteRecord = async (id) => {
-      console.log('to delete: ', id);
-      await axios.delete(`https://speelplenapi.onrender.com/record/deleteAStat/${id}`);
-      getAllStats();
-    };
-
-    const deleteAllRecord = async () => {
-      await axios.delete('https://speelplenapi.onrender.com/record/deleteAllStats');
-      getAllStats();
-    };
-
-    useEffect(() => {
-      getAllStats();
-    }, []);
-
-    const handleDelete = (id) => {
-      if (window.confirm('Are you sure you want to DELETE?')) {
-        deleteRecord(id);
-      } else {
-      }
-      getAllStats();
-    };
-    const handleDeleteAll = () => {
-      if (window.confirm('Are you sure you want to DELETE all the Stats?')) {
-        deleteAllRecord();
-      } else {
-      }
-      getAllStats();
-    };
-
-    const handleSearchName = (event) => {
-      let value = event.target.value.toLowerCase();
-      let result = [];
-      console.log(value);
-      result = childs.filter((data) => {
-        return data.name?.toLowerCase().includes(value.toLowerCase());
+  const [childs, setChild] = useState([]);
+  const [filteredData, setFilteredData] = useState(childs);
+  const getAllStats = async () => {
+    await axios
+      .get("https://speelplenapi.onrender.com/record/stats/")
+      .then((response) => {
+        setChild(response.data);
+        setFilteredData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-      //child.name?.toLowerCase().includes(query.toLowerCase())
-      
-      setFilteredData(result);
-    };
+  };
+  const deleteRecord = async (id) => {
+    await axios.delete(
+      `https://speelplenapi.onrender.com/record/deleteAStat/${id}`
+    );
+    getAllStats();
+  };
 
-    const handleSearchDate = (event) => {
-      let value = event.target.value.toLowerCase();
-      let result = [];
-      console.log(value);
-      result = childs.filter((data) => {
-        return data.date.indexOf(value) !== -1;
-      });
-      
-      setFilteredData(result);
-    };
+  const deleteAllRecord = async () => {
+    await axios.delete(
+      "https://speelplenapi.onrender.com/record/deleteAllStats"
+    );
+    getAllStats();
+  };
+
+  useEffect(() => {
+    getAllStats();
+  }, []);
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to DELETE?")) {
+      deleteRecord(id);
+    } else {
+    }
+    getAllStats();
+  };
+  const handleDeleteAll = () => {
+    if (window.confirm("Are you sure you want to DELETE all the Stats?")) {
+      deleteAllRecord();
+    } else {
+    }
+    getAllStats();
+  };
+
+  const handleSearchName = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = childs.filter((data) => {
+      return data.name?.toLowerCase().includes(value.toLowerCase());
+    });
+    //child.name?.toLowerCase().includes(query.toLowerCase())
+
+    setFilteredData(result);
+  };
+
+  const handleSearchDate = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = childs.filter((data) => {
+      return data.date.indexOf(value) !== -1;
+    });
+
+    setFilteredData(result);
+  };
 
   if (childs.length === 0) {
-    return <Spinner />
+    return <Spinner />;
   }
   return (
     <div>
-      <div className='table__container'>
-        <h3 className='text-center py-3 table__text'>Dagelijkse Registraties</h3>
-        <div className='row'>
-          <div className='col-4'>
+      <div className="table__container">
+        <h3 className="text-center py-3 table__text">
+          Dagelijkse Registraties
+        </h3>
+        <div className="search-bar">
+          <div className="search-bar1">
             <input
-              className='form-control'
-              placeholder='zoek op naam'
-              type='text'
+              className="form-control"
+              placeholder="zoek op naam"
+              type="text"
               onChange={(event) => handleSearchName(event)}
             />
-          </div>
-          <div className='col-4'>
             <input
-              className='form-control'
-              placeholder='zoeken op datum'
-              type='text'
+              className="form-control"
+              placeholder="zoeken op datum"
+              type="text"
               onChange={(event) => handleSearchDate(event)}
             />
           </div>
-        </div>
-        <div className='to-excel1'>
           <ReactHTMLTableToExcel
-            className='btn btn-primary '
-            table='emp-table'
-            filename='Emp Excel file'
-            sheet='Sheet'
-            buttonText='Exporteren naar Excel'
+            className="btn btn-primary "
+            table="emp-table"
+            filename="Emp Excel file"
+            sheet="Sheet"
+            buttonText="Exporteren naar Excel"
           />
         </div>
 
-        <div className='table table-responsive table__wrapper1'>
+        <div className="table table-responsive table__wrapper1">
           <table
-            className='table table-striped table-bordered table-hover mt-5 c'
-            id='emp-table'
+            className="table table-striped table-bordered table-hover mt-5 c"
+            id="emp-table"
             style={{ marginTop: 20 }}
           >
-            <thead className='table-dark'>
+            <thead className="table-dark">
               <tr>
-                <th style={{ width: '5%' }}>#</th>
-                <th style={{ width: '45%' }}>Achetenaam en Voornaam</th>
-                <th style={{ width: '8%' }}>Saldo</th>
-                <th style={{ width: '20%' }}>Sociaal</th>
-                <th style={{ width: '15%' }}>Datum</th>
-                <th style={{ width: '15%' }}><Link to="/dailyR"><IoIosTrash className='icon-two cursor-pointer' onClick={() => handleDeleteAll()} /></Link></th>
+                <th style={{ width: "5%" }}>#</th>
+                <th style={{ width: "45%" }}>Achetenaam en Voornaam</th>
+                <th style={{ width: "8%" }}>Saldo</th>
+                <th style={{ width: "20%" }}>Sociaal</th>
+                <th style={{ width: "15%" }}>Datum</th>
+                <th style={{ width: "15%" }}>
+                  <Link to="/dailyR">
+                    <IoIosTrash
+                      className="icon-two cursor-pointer"
+                      onClick={() => handleDeleteAll()}
+                    />
+                  </Link>
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -144,10 +150,10 @@ const Stats = () => {
                     </td> */}
                     <td>
                       <Link
-                        to={'/dailyR'}
+                        to={"/dailyR"}
                         onClick={() => handleDelete(child._id)}
                       >
-                        <IoIosTrash className='icon-two' />
+                        <IoIosTrash className="icon-two" />
                       </Link>
                     </td>
                   </tr>
